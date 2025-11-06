a20c301ae40934410f3c39685865b9c6da0a42de
mkummer <mario.kummer@students.unibe.ch>
2025-11-06 12:10:10 +0100

    Working bash script for fastqc analysis

--- scripts/fastqc_analysis.sh
+++ scripts/fastqc_analysis.sh
@@ -7,19 +7,23 @@
 #SBATCH --job-name=fastqc_analysis
 #SBATCH --mail-user=mario.kummer@students.unibe.ch
 #SBATCH --mail-type=BEGIN,END
-#SBATCH --output=/data/users/mkummer/RNA-Sequencing-Course/results/fastqc/fastqc_analysis_quality_check_%j.o
-#SBATCH --error=/data/users/mkummer/RNA-Sequencing-Course/results/fastqc/fastqc_analysis_error_%j.e
+#SBATCH --output=/data/users/mkummer/RNA-Sequencing-Course/logs/fastqc_analysis_quality_check_%j.o
+#SBATCH --error=/data/users/mkummer/RNA-Sequencing-Course/logs/fastqc_analysis_error_%j.e
+
+module load FastQC/0.11.9-Java-11
 
 # --- User-configurable defaults ---
-READS_DIR="/data/course/data/courses/rnaseq_course/toxoplasma_de/reads_Lung"
-RESULTS_DIR="/data/users/mkummer/RNA-Sequencing-Course/results/fastqc"
+READS_DIR="/data/courses/rnaseq_course/toxoplasma_de/reads_Lung"
+RESULTS_DIR="/data/users/mkummer/RNA-Sequencing-Course/results"
 SIF_PATH="/containers/apptainer/fastqc-0.12.1.sif"
-FASTQ_PATTERN="${1:-SRR_ll*.fastq*}"
+FASTQ_PATTERN="*.fastq.*"
 
-# Create results directory
 mkdir -p "$RESULTS_DIR"
 
-# Run FastQC inside the container on matching files
-apptainer exec "$SIF_PATH" fastqc -t 2 -o "$RESULTS_DIR" "$READS_DIR"/$FASTQ_PATTERN
-
-
+for file in "$READS_DIR"/$FASTQ_PATTERN; do
+  echo "Processing $file..."
+  apptainer exec \
+  --bind "$READS_DIR":"$READS_DIR" \
+  --bind "$RESULTS_DIR":"$RESULTS_DIR" \
+  "$SIF_PATH" fastqc -t 2 -o "$RESULTS_DIR" "$file"
+done
